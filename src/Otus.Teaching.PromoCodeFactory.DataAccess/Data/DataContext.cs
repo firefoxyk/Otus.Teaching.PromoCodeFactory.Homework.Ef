eba0ccc443b810742d21ea04b87caca92992fa00
Машулька Машулька <marussia01@gmail.com>
2023-09-20 15:04:09 +0300

    Добавить Entity Framework Core в DataAccess проект и сделать реализацию IRepository в виде EfRepository, которая будет работать с DataContext Entity Framework. Добавить SQLite в качестве БД

---
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otus.Teaching.PromoCodeFactory.DataAccess.Data
+{
+    public class DataContext : DbContext
+    {
+
+    }
+
+}
---
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Data/EfDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otus.Teaching.PromoCodeFactory.DataAccess.Data
+{
+    internal class EfDbInitializer
+    {
+    }
+}
